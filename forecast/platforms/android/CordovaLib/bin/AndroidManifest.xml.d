c9c5d13aabba592f5dd36b24f855ea426ddd6948
FadiSh <fadi.shazbek@gmail.com>
2015-03-12 13:23:49 +0200

    2
    2

---
+++ forecast/platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+F:\WORK\devcolibri-lessons\Android_Web-app\WeatherChecker\platforms\android\CordovaLib\bin\AndroidManifest.xml : \
+F:\WORK\devcolibri-lessons\Android_Web-app\WeatherChecker\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ forecast/platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : F:\WORK\devcolibri-lessons\Android_Web-app\WeatherChecker\platforms\android\CordovaLib\bin\AndroidManifest.xml \
